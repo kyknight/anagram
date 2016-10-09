d0051fd0bd3f8b39ed0edcdb0fb66ee7349c13b9
Kyleigh Knight <kyleigh.knight@hotmail.com>
2016-10-09 17:27:11 -0500

    Delete reverse_letters.rb

--- reverse_letters.rb
+++
@@ -1,9 +0,0 @@
-def reverse_letters (letters)
-	length= letters.length
-	reverse_letters = Array.new(length)
-
-	letters.each_with_index do |letter, index|
-		reverse_letters[length - index - 1] = letter
-	end
-	reverse_letters
-end
